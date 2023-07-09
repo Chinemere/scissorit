1750854fc19aa087f1f55ee331ce8239ce224e22
Chinemere Chukwu <chukwuchinemere@hmail.com>
2023-07-09 19:24:38 +0100

    updated main.py

--- main.py
+++ main.py
@@ -49,6 +49,12 @@ def create_app():
 app = create_app()
 
 
+# if __name__=='__main__':
+    
+#     app.run(debug=True, port=8000)
+
+
+
 @app.route('/')
 def index():
      return render_template('index.html')
@@ -161,6 +167,6 @@ def logout():
 
 
 
-# if __name__=='__main__':
+if __name__=='__main__':
     
-#     app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000)
